1ce775c9f2f6f0bba001f1ee75cf45d2a859b6fe
lvlON-Emperor <46490084+MonEmperor@users.noreply.github.com>
2022-05-18 23:31:53 -0400

    made the get, update, delete and reset tile functions.
    will start the methods for working with multiple(or even all) tiles tomorrow probably.
    
    also need to add unit tests once i figure out JS importing files.

--- src/db/tiles.js
+++ src/db/tiles.js
@@ -54,7 +54,7 @@ async function template(){
     }
 }
 
-async function createTile(client, tile) {
+async function createTile(tile) {
     /*
     * Adds a tile to the database.
     *
@@ -65,16 +65,55 @@ async function createTile(client, tile) {
     *   last_modifier: str  // the user who last modified the colour
     * }
     *
+    * Example:
+    *   // creates a single tile at position [-1,-1], with the colour, black.
+    *   // we do not need to set a last_modifier, as nobody has modified it yet.
+    *   >>>createTile({
+    *       position: [-1,-1],
+    *       colour: [0,0,0]
+    *   })
     */
+
+    // keep the client here. this way we can have multiple calls using client. without worrying if it is open or closed
+    const client = new MongoClient(uri)
+
+    // tries to connect and run the operation
+    try {
+        await client.connect() // connects
+
+        const result = await client.db("db").collection("tiles").insertOne(tile) // adds the tile
+
+        console.log(`Created a new tile with ID: ${result.insertedId}`)
+
+    } catch (e) { // because it is async, it may fail in that case, we log the error
+        console.error(e)
+
+    } finally { // in any and all cases, close the connection afterward
+        await client.close()
+
+    }
+
 }
 
-async function createTiles(client, tiles) {}
+async function createTiles(tiles) {}
 
-async function updateTile(client, position, updated_fields) {
+async function updateTile(position, updated_fields) {
     /*
     * Updates a tile.
     *
     * searches for a tile by its position. then it updates the tile's colour and the last user who modified it.
+    *
+    * We search for the tile by its position, and then insert the updated fields such as colour or the last_modifier
+    *
+    * Examples:
+    *   // updates the tile at [-1,-1] tp the colour black. this change was made by user, "lvlonEmperor"
+    *   updateTile(
+    *       [-1,-1],
+    *       {
+    *           colour: [0,0,0],
+    *           last_modifier= "lvlonEmperor"
+    *       }
+    *   )
     *
     * updated_fields:
     * {
@@ -82,9 +121,33 @@ async function updateTile(client, position, updated_fields) {
     *   last_modifier: ""
     * }
     */
+
+    const client = new MongoClient(uri)
+
+    // tries to connect and run the operation
+    try {
+        await client.connect() // connects
+
+        // searches for the matching tile and changes the fields
+        console.log(position)
+        const result = await client.db("db").collection("tiles").updateOne(
+            {position: position},
+            {$set: updated_fields}
+        )
+
+        console.log("matching documents: " + result.matchedCount)
+        console.log(result.modifiedCount + " docs were updated")
+
+    } catch (e) { // because it is async, it may fail in that case, we log the error
+        console.error(e)
+
+    } finally { // in any and all cases, close the connection afterward
+        await client.close()
+
+    }
 }
 
-async function upsertDB(client) {
+async function upsertDB() {
     /*
     * Used to create the database.
     *
@@ -92,23 +155,52 @@ async function upsertDB(client) {
     */
 }
 
-async function resetTile(client) {
+async function resetTile(position) {
     /*
     * Resets a tile to the default setting.
     *
-    * essentially just updates the colour to white and clears the last_modifier
+    * essentially just updates the colour to white and clears the last_modifier.
+    *
+    * Does this by calling the updateTile() method
+    *
+    * Example:
+    *   // updates the tile at 0,0 to the colour white.
+    *   resetTile([0,0])
     */
+    updateTile(position, {colour: [255,255,255]})
 }
 
-async function deleteTile(client, position) {
+async function deleteTile(position) {
     /*
     * Deletes a tile.
     *
     * Meant for development, not production.
+    *
+    * Examples:
+    *   // deletes the tile at position [-1,-1]
+    *   deleteTile([-1, -1])
     */
+
+    const client = new MongoClient(uri)
+
+    // tries to connect and run the operation
+    try {
+        await client.connect() // connects
+
+        const result = await client.db("db").collection("tiles").deleteOne({position: position})
+
+        console.log(result.deletedCount + " docs were deleted")
+
+    } catch (e) { // because it is async, it may fail in that case, we log the error
+        console.error(e)
+
+    } finally { // in any and all cases, close the connection afterward
+        await client.close()
+
+    }
 }
 
-async function deleteAll(client) {
+async function deleteAll() {
     /*
     * Deletes all tiles.
     *
@@ -116,19 +208,57 @@ async function deleteAll(client) {
     */
 }
 
-async function getTile(client, position) {
+async function getTile(position) {
     /*
     * Gets a single tile from the db.
     * */
+
+    const client = new MongoClient(uri)
+
+    // tries to connect and run the operation
+    try {
+        await client.connect() // connects
+
+        const result = await client.db("db").collection("tiles").findOne({position: position})
+
+        if (result) {
+            console.log("Found result")
+            console.log(result)
+        } else {
+            console.log("No listings found")
+        }
+
+        return result
+
+    } catch (e) { // because it is async, it may fail in that case, we log the error
+        console.error(e)
+
+    } finally { // in any and all cases, close the connection afterward
+        await client.close()
+
+    }
 }
 
-async function getAllTiles(client) {
+async function getAllTiles() {
     /*
     * Gets all tiles from the db.
     *
     * Used very often. meant to load the webpage(and all the tiles in the db) as well as
     * reload the page contents(the tiles) every second or so.
+    *
+    * TODO:
+    *   see if there is a built in get all. same for delete.
     * */
+
 }
 
+<<<<<<< Updated upstream
 //template().catch(console.error)
+=======
+
+//createTile({position: [-1,-1], colour: [255,255,255]})
+//updateTile([-1,-1] , {colour: [0,0,1], last_modifier: "lvlonEmperor"})//.catch(console.error)
+//resetTile([-1,-1])
+//deleteTile([-1,-1])
+//console.log(getTile([-1,-1]))
+>>>>>>> Stashed changes
