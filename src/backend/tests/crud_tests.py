325a2fdcf2cb9a0e38feffcae5646ace78ecd6b5
MonEmperor <abr.da.king2016@gmail.com>
2022-06-03 03:14:35 -0400

    deploy attempt 3

--- src/backend/tests/crud_tests.py
+++ src/backend/tests/crud_tests.py
@@ -220,9 +220,9 @@ def deleteAll(): pass # probably could be turned into an API call.
 
 
 if __name__ == '__main__': # uncomment each line to test out a feature. only 1 api call at a time tho. async?
+    createTile([2, 1], [0, 0, 0])
     getTile([2,1])
     #getAllTiles()
-    #createTile([2, 1], [0, 0, 0])
     #updateTile([2,1], [25,25,54], last_modifier="you")
     #resetTile([2, 1])
     #deleteTile([2,1])
